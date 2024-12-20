520eaca74f8720dcd6cbe0de31c3fb4df6878ab5
msetu1 <msetu5763@gmail.com>
2024-12-20 14:30:23 +0600

    academic department a schema model add

---
+++ src/module/academicDepartment/academicDepartment.validate.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod';
+
+export const createAcademicDepartmentValidateSchema = z.object({
+  name: z.string({
+    invalid_type_error: 'Academic Department must be string',
+    required_error: 'name is required',
+  }),
+  academicFaculty: z.string({
+    invalid_type_error: 'Academic Department must be string',
+    required_error: 'Faculty is required',
+  }),
+});
+export const updateAcademicDepartmentValidateSchema = z.object({
+  name: z
+    .string({
+      invalid_type_error: 'Academic Department must be string',
+      required_error: 'name is required',
+    })
+    .optional(),
+  academicFaculty: z
+    .string({
+      invalid_type_error: 'Academic Department must be string',
+      required_error: 'Faculty is required',
+    })
+    .optional(),
+});
+
+export const AcademicFacultyValidate = {
+  createAcademicDepartmentValidateSchema,
+  updateAcademicDepartmentValidateSchema,
+};

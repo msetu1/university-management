f8877aa0f9f0d6c135da475829e4c94040c33401
msetu1 <msetu5763@gmail.com>
2024-12-23 22:55:36 +0600

    offered course schema add

---
+++ src/module/OfferdCourse/OfferdCourse.interface.ts
@@ -0,0 +1,25 @@
+import { Types } from 'mongoose';
+
+export type Days =
+  | 'Sat'
+  | 'Sun'
+  | 'Mon'
+  | 'Tue'
+  | 'Wed'
+  | 'Thu'
+  | 'Fri'
+  | 'Sat';
+
+export type TOfferedCourse = {
+  semesterRegister: Types.ObjectId;
+  academicSemester: Types.ObjectId;
+  academicFaculty: Types.ObjectId;
+  academicDepartment: Types.ObjectId;
+  course: Types.ObjectId;
+  faculty: Types.ObjectId;
+  maxCapacity: number;
+  section: number;
+  days: Days;
+  startTime: string;
+  endTime: string;
+};
---
+++ src/module/OfferdCourse/OfferdCourse.model.ts
@@ -0,0 +1,54 @@
+import { model, Schema } from 'mongoose';
+import { TOfferedCourse } from './OfferdCourse.interface';
+import { Days } from './OfferedCourse.constant';
+
+const offeredCourseSchema = new Schema<TOfferedCourse>(
+  {
+    semesterRegister: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'SemesterRegister',
+    },
+    academicSemester: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'AcademicSemester',
+    },
+    academicFaculty: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'AcademicFaculty',
+    },
+    academicDepartment: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'AcademicDepartment',
+    },
+    course: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'Course',
+    },
+    faculty: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'Faculty',
+    },
+    maxCapacity: { type: Number, required: true },
+    section: { type: Number, required: true },
+    days: {
+      type: String,
+      enum: Days,
+    },
+    startTime: { type: String, required: true },
+    endTime: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const OfferedCourse = model<TOfferedCourse>(
+  'OfferedCourse',
+  offeredCourseSchema,
+);
